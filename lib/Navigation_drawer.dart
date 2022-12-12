67d1e8385db201453463ae59548e923acf257d30
Sumit <sumitsinha401@gmail.com>
2022-12-12 15:59:51 +0530

    Version 1.0

--- lib/Navigation_drawer.dart
+++ lib/Navigation_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:project/basic.dart';
 import 'package:project/converters_file.dart';
 import 'package:project/scientific.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Navigation_drawer extends StatelessWidget {
   const Navigation_drawer({Key? key}) : super(key: key);
@@ -41,7 +42,8 @@ class Navigation_drawer extends StatelessWidget {
           ),
           ListTile(
             contentPadding: EdgeInsets.only(top: 20, left: 20),
-            leading: Icon(Icons.calculate),
+            leading: Icon(FontAwesomeIcons.calculator,size: 20,
+                color: Colors.black),
             title: Text(
               "Basic Calculator",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
@@ -53,7 +55,8 @@ class Navigation_drawer extends StatelessWidget {
           ),
           ListTile(
             contentPadding: EdgeInsets.only(top: 20, left: 20),
-            leading: Icon(Icons.calculate),
+            leading: Icon(FontAwesomeIcons.squareRootVariable,size: 20,
+              color: Colors.black,),
             title: Text(
               "Scientific Calculator",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
@@ -65,9 +68,23 @@ class Navigation_drawer extends StatelessWidget {
           ),
           ListTile(
             contentPadding: EdgeInsets.only(top: 20, left: 20),
-            leading: Icon(Icons.calculate),
+            leading: Icon(FontAwesomeIcons.leftRight,size: 20,
+              color: Colors.black,),
             title: Text(
               "Converter",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+            ),
+            onTap: () => {
+              Navigator.pop(context),
+              Navigator.of(context).pushNamed(Converters_File.route),
+            },
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.only(top: 20, left: 20),
+            leading: Icon(FontAwesomeIcons.circleInfo,size: 30,
+                color: Colors.black),
+            title: Text(
+              "About Us",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
             ),
             onTap: () => {
